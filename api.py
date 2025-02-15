15ac71a113a6925537d13b807ec8b0665d66f650
josephsasaki <joseph.sasaki.314@gmail.com>
2025-02-15 18:54:16 +0000

    Changed port to 5000

--- api.py
+++ api.py
@@ -16,4 +16,4 @@ def endpoint_index():
 if __name__ == "__main__":
     app.config['TESTING'] = True
     app.config['DEBUG'] = True
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=5000)
