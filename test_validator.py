f81acf97325e51875d9f0e57c8335a3bff0649d6
josephsasaki <joseph.sasaki.314@gmail.com>
2025-03-07 11:21:57 +0000

    Completed testing of validator functions

--- test_validator.py
+++ test_validator.py
@@ -1,73 +1,73 @@
 # pylint: disable=unused-variable
 '''Test api file'''
 import pytest
-from validator import is_valid_word, is_valid_scrabble_word
+from validator import is_valid_word, is_scrabble_word
 
 
-VALID_WORDS = ["alfa", "bravo", "charlie", "delta", "echo", "foxtrot",
-               "golfer", "hotel", "inhale", "juliennes", "kilo", "lima", "mike",
-               "novemdecillion", "oscillate", "papa", "queen", "romeo", "sierra",
-               "tango", "uniform", "victor", "whiskey", "xanthan", "yanked", "zucchetto"]
-INVALID_WORDS = ["axxxxx", "bxxxxx", "cxxxxx", "dxxxxx", "exxxxx", "fxxxxx",
-                 "gxxxxx", "hxxxxx", "ixxxxx", "jxxxxx", "kxxxxx", "lxxxxx", "mxxxxx",
-                 "nxxxxx", "oxxxxx", "pxxxxx", "qxxxxx", "rxxxxx", "sxxxxx",
-                 "txxxxx", "uxxxxx", "vxxxx", "wxxxxx", "xxxxxx", "yxxxxx", "zxxxxx"]
-VALID_WORDS_REVERSED = [word[::-1] for word in VALID_WORDS]
-INVALID_WORDS_REVERSED = [word[::-1] for word in INVALID_WORDS]
-INVALID_ARGUMENTS = ["1234", "ab473", "_387f?", "furf ",
-                     "03[]", "@fvvrc", "az ajqoq", "ffefe.", " "]
-CAPITALISED_VALID_WORDS = [word.upper() for word in VALID_WORDS]
+VALID_SCRABBLE = [
+    "alfa", "bravo", "charlie", "delta", "echo", "foxtrot",
+    "golfer", "hotel", "inhale", "juliennes", "kilo", "lima", "mike",
+    "nothing", "oscillate", "papa", "queen", "romeo", "sierra",
+    "tango", "uniform", "victor", "whiskey", "xylem", "yanked", "zucchetto"
+]
+INVALID_SCRABBLE = [
+    "axxxxx", "bxxxxx", "cxxxxx", "dxxxxx", "exxxxx", "fxxxxx",
+    "gxxxxx", "hxxxxx", "ixxxxx", "jxxxxx", "kxxxxx", "lxxxxx", "mxxxxx",
+    "nxxxxx", "oxxxxx", "pxxxxx", "qxxxxx", "rxxxxx", "sxxxxx",
+    "txxxxx", "uxxxxx", "vxxxx", "wxxxxx", "xxxxxx", "yxxxxx", "zxxxxx"
+]
+INVALID_WORD = [
+    "As45", "56dQ", "hel lo", "joy_", "insane$", "?wow"
+]
+PALINDROMES = [
+    "aha", "bob", "civic", "deed", "eve", "gig", "kayak", "level", "madam",
+    "noon", "otto", "pip", "radar", "sagas", "tenet", "ulu", "vav", "wow", "yay"
+]
 
 
-@pytest.mark.parametrize("valid_argument", VALID_WORDS + INVALID_WORDS + CAPITALISED_VALID_WORDS)
-def test_valid_argument(valid_argument):
-    '''Test valid argument return true'''
-    assert is_valid_word(valid_argument) == True
+# --- IS_VALID_WORD ---
 
 
-@pytest.mark.parametrize("invalid_argument", INVALID_ARGUMENTS)
-def test_invalid_argument(invalid_argument):
-    '''Test invalid argument return false'''
-    assert is_valid_word(invalid_argument) == False
+@pytest.mark.parametrize("valid_word", VALID_SCRABBLE + INVALID_SCRABBLE)
+def test_is_valid_word_valid(valid_word):
+    '''Test valid words return true'''
+    assert is_valid_word(valid_word) == True
 
 
-@pytest.mark.parametrize("valid_word", VALID_WORDS)
-def test_valid_word_no_reverse_returns_true(valid_word):
-    '''Test valid word returns true (when not checking for reverse)'''
-    assert is_valid_scrabble_word(valid_word, False) == True
+@pytest.mark.parametrize("invalid_word", INVALID_WORD)
+def test_is_valid_word_invalid(invalid_word):
+    '''Test invalid words return false'''
+    assert is_valid_word(invalid_word) == False
 
 
-@pytest.mark.parametrize("valid_word_reversed", VALID_WORDS_REVERSED)
-def test_valid_word_reversed_no_reverse_returns_false(valid_word_reversed):
-    '''Test valid words reversed returns false (when not checking for reverse)'''
-    assert is_valid_scrabble_word(valid_word_reversed, False) == False
+@pytest.mark.parametrize("valid_capitalised_word", map(lambda x: x.upper(), VALID_SCRABBLE))
+def test_is_valid_word_capitalised(valid_capitalised_word):
+    '''Test capitalised valid words return true'''
+    assert is_valid_word(valid_capitalised_word) == True
 
 
-@pytest.mark.parametrize("invalid_word", INVALID_WORDS)
-def test_invalid_word_no_reverse_returns_false(invalid_word):
-    '''Test invalid word returns false (when not checking for reverse)'''
-    assert is_valid_scrabble_word(invalid_word, False) == False
+# --- IS_SCRABBLE_WORD ---
 
 
-@pytest.mark.parametrize("valid_word_reversed", VALID_WORDS_REVERSED)
-def test_valid_word_reversed_check_reverse_returns_true(valid_word_reversed):
-    '''Test valid word reversed returns true (when checking for reverse)'''
-    assert is_valid_scrabble_word(valid_word_reversed, True) == True
+@pytest.mark.parametrize("palindromes", PALINDROMES)
+def test_is_scrabble_word_true_true(palindromes):
+    '''Test palindromes return true for forwards and reversed'''
+    assert is_scrabble_word(palindromes) == [True, True]
 
 
-@pytest.mark.parametrize("invalid_word_reversed", INVALID_WORDS_REVERSED)
-def test_invalid_word_reversed_check_reverse_returns_false(invalid_word_reversed):
-    '''Test invalid word reversed returns false (when checking for reverse)'''
-    assert is_valid_scrabble_word(invalid_word_reversed, True) == False
+@pytest.mark.parametrize("invalid_scrabble", INVALID_SCRABBLE)
+def test_is_scrabble_word_false_false(invalid_scrabble):
+    '''Test invalid scrabbles return false for forwards and reversed'''
+    assert is_scrabble_word(invalid_scrabble) == [False, False]
 
 
-@pytest.mark.parametrize("capitalised_valid_word", CAPITALISED_VALID_WORDS)
-def test_valid_word_capitalised_no_reverse_returns_true(capitalised_valid_word):
-    '''Test invalid word reversed returns false (when checking for reverse)'''
-    assert is_valid_scrabble_word(capitalised_valid_word, False) == True
+@pytest.mark.parametrize("valid_forwards", VALID_SCRABBLE)
+def test_is_scrabble_word_true_false(valid_forwards):
+    '''Test invalid scrabble words return true forwards and false backwards'''
+    assert is_scrabble_word(valid_forwards) == [True, False]
 
 
-@pytest.mark.parametrize("capitalised_valid_word", CAPITALISED_VALID_WORDS)
-def test_valid_word_capitalised_check_reverse_returns_true(capitalised_valid_word):
-    '''Test invalid word reversed returns false (when checking for reverse)'''
-    assert is_valid_scrabble_word(capitalised_valid_word, True) == True
+@pytest.mark.parametrize("valid_reversed", map(lambda x: x[::-1], VALID_SCRABBLE))
+def test_is_scrabble_word_true_false(valid_reversed):
+    '''Test invalid scrabble words return true forwards and false backwards'''
+    assert is_scrabble_word(valid_reversed) == [False, True]
